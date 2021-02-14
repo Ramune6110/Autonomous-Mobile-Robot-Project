eb9d1a68a914a690f2647e17898658c43d359652
Ramune6110 <arduino6110@gmail.com>
2021-02-14 05:55:26 -0800

    [modify]following_people

--- src/fail_safe/src/fail_safe_following_people.cpp
+++ src/fail_safe/src/fail_safe_following_people.cpp
@@ -22,6 +22,87 @@ void scan_callback(const sensor_msgs::LaserScan::ConstPtr &msg)
     latest_scan = *msg;
 }
 
+// Monitoring +5° in front
+int8_t observationFront_positive_5_degree()
+{
+    int count = 0;
+
+    for(int i = 0; i < 10; i++) {
+        /**
+        ***********************************************************************
+        * First condition  |  In case of an error value
+        * Second condition |  Out of measurement range
+        * Third condition  |  For infinity
+        ***********************************************************************
+        */
+        if (latest_scan.ranges[i] < latest_scan.range_min || 
+            latest_scan.ranges[i] > latest_scan.range_max || 
+            std::isnan(latest_scan.ranges[i])) 
+        {
+            // 測定範囲外 = 正面に十分な距離があるわけではない。(場合によっては、とても近い距離かもしれない)
+            //ROS_INFO("i = %d, front-range: measurement error", i);
+        } 
+        else 
+        {
+            //ROS_INFO("i = %d, front-range: %0.3f", i, latest_scan.ranges[i]);
+            if (latest_scan.ranges[i] > 0.3) {
+                // 20cm以上距離がある
+                ;
+            } else {
+                // 20cm以下になった
+                count += 1;
+            }
+        }
+    }
+
+    return count;
+}
+
+// Monitoring -5° in front
+int8_t observationFront_negative_5_degree()
+{
+    int count = 0;
+
+    for(int i = 350; i < 360; i++) {
+        /**
+        ***********************************************************************
+        * First condition  |  In case of an error value
+        * Second condition |  Out of measurement range
+        * Third condition  |  For infinity
+        ***********************************************************************
+        */
+        if (latest_scan.ranges[i] < latest_scan.range_min || 
+            latest_scan.ranges[i] > latest_scan.range_max || 
+            std::isnan(latest_scan.ranges[i])) 
+        {
+            // 測定範囲外 = 正面に十分な距離があるわけではない。(場合によっては、とても近い距離かもしれない)
+            //ROS_INFO("i = %d, front-range: measurement error", i);
+        } 
+        else 
+        {
+            //ROS_INFO("i = %d, front-range: %0.3f", i, latest_scan.ranges[i]);
+            if (latest_scan.ranges[i] > 0.3) {
+                // 20cm以上距離がある
+                ;
+            } else {
+                // 20cm以下になった
+                count += 1;
+            }
+        }
+    }
+
+    return count;
+}
+
+// Monitoring ±5° in front
+int8_t obstacle_detection()
+{
+    int8_t positive_count = observationFront_positive_5_degree();
+    int8_t negative_count = observationFront_negative_5_degree();
+
+    return positive_count + negative_count;
+}
+
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "fail_safe_following_people_node");
@@ -91,12 +172,19 @@ int main(int argc, char** argv)
                 cmd_vel.linear.x = sqrtf(2.0 * 0.3 * fabs(dist - 0.5));
 
                 if (dist - 0.5 < 0) {
-                    cmd_vel.linear.x *= -1;
+                    //cmd_vel.linear.x *= -1;
+                    cmd_vel.linear.x *= 1;
                 }
                 if (cmd_vel.linear.x > 0.2) {
                     cmd_vel.linear.x = 0.2;
                 } else if (cmd_vel.linear.x < -0.2) {
                     cmd_vel.linear.x = -0.2;
+                }
+
+                if(obstacle_detection() >= 3) {
+                    ROS_INFO("STOP!!");
+                    cmd_vel.linear.x  = 0.0;
+                    cmd_vel.angular.z = 0.0;
                 }
             }
 
